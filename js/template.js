6e5b5a04b8cb40954ab2a977901abc2576df26ef
H-ZT-270 <27011240@qq.com>
2019-04-09 18:03:32 +0800

    首首页开发

--- js/template.js
+++ js/template.js
@@ -26,11 +26,26 @@ Vue.component('v-footer', {
 
 Vue.component('data-list', {
 
-	props: ['article'],
+	props: ['list'],
 	data: function (props) {
+	    
+	    var seen;
+	  	var content = props.list;
+	  	if(content == ''){
 
-		
+	  		return {
+	  			seen : false
+	  		}
 
-	}
+	  	}else{
+
+	  		return {
+	  			seen : true
+	  		}
+
+	  	}
+
+	},
+	template: '<ul class="data-list" v-if="seen"><li  v-for="v in list"><span class="time">{{ v.time }}</span><span class="article"><h4>{{ v.title }}</h4><p>{{ v.remark }}</p><a v-bind:href="v.href">查看更多</a></span></li></ul>',
 
 })
